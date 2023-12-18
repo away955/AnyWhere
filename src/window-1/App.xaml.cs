4b250ee36d575e8b93018e9156fc967e18eb67a6
away.wei <1461076997@qq.com>
2023-12-18 19:29:02 +0800

    Window-1：可收缩菜单

--- src/window-1/App.xaml.cs
+++ src/window-1/App.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace Away.Windows
+namespace Window1
 {
     /// <summary>
     /// Interaction logic for App.xaml

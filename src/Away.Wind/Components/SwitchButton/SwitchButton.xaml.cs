0806b7ee9875d1aa2b2e79969b6d2eb147aed4b5
away.wei <1461076997@qq.com>
2024-01-31 15:23:45 +0800

    样式细节优化

---
+++ src/Away.Wind/Components/SwitchButton/SwitchButton.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace Away.Wind.Components;
+
+/// <summary>
+/// SwitchButton.xaml 的交互逻辑
+/// </summary>
+public partial class SwitchButton : UserControl
+{
+    public SwitchButton()
+    {
+        InitializeComponent();
+    }
+
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    public bool IsChecked
+    {
+        get { return (bool)GetValue(IsCheckedProperty); }
+        set { SetValue(IsCheckedProperty, value); }
+    }
+
+
+    public readonly static DependencyProperty TextProperty;
+    public readonly static DependencyProperty IsCheckedProperty;
+    static SwitchButton()
+    {
+        TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(SwitchButton), new PropertyMetadata(string.Empty, new PropertyChangedCallback(OnTextChanged)));
+        IsCheckedProperty = DependencyProperty.Register(nameof(IsChecked), typeof(bool), typeof(SwitchButton), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    }
+
+    private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not SwitchButton control)
+        {
+            return;
+        }
+        control.TxtLabel.Text = Convert.ToString(e.NewValue);
+    }
+
+    private void CBSwitchBtn_Checked(object sender, RoutedEventArgs e)
+    {
+        this.IsChecked = true;
+        TxtCheckLabel.Text = "开";
+    }
+
+    private void CBSwitchBtn_Unchecked(object sender, RoutedEventArgs e)
+    {
+        this.IsChecked = false;
+        TxtCheckLabel.Text = "关";
+    }
+}

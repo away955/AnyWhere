5bc9411ddc5a1e85c9720cf32188d0e123c02741
away.wei <1461076997@qq.com>
2024-03-04 18:30:58 +0800

    修改readme

---
+++ src/Away.App.Core/Windows/ProcessOnly/Impl/LinuxProcessOnly.cs
@@ -0,0 +1,9 @@
+﻿namespace Away.App.Core.Windows.ProcessOnly.Impl;
+
+public sealed class LinuxProcessOnly : IProcessOnly
+{
+    public bool Show(string mutexName, bool isShow = true)
+    {
+        return false;
+    }
+}
---
+++ src/Away.App.Core/Windows/ProcessOnly/Impl/MacOSProcessOnly.cs
@@ -0,0 +1,9 @@
+﻿namespace Away.App.Core.Windows.ProcessOnly.Impl;
+
+public sealed class MacOSProcessOnly : IProcessOnly
+{
+    public bool Show(string mutexName, bool isShow = true)
+    {
+        return false;
+    }
+}
